579670bd9ec98a6d6cdc3373e573869bcd515c84
drjofu <drjofu@web.de>
2018-12-13 15:30:44 +0100

    x

--- SignalrNgC5Client/src/app/app.config.ts
+++
@@ -1,6 +0,0 @@
-export class AppConfig{
-   public baseUrl = "https://localhost:5001/";
- // public baseUrl = "https://localhost:44387/";
-  public chatHubUrl = this.baseUrl + "chat"; 
-  public cityHubUrl = this.baseUrl + "city";
-}
